34433545e885f4e73f1b5531349e9f30b8b895a2
Gustavo Araújo <gustavoaraujo.dev@gmail.com>
2023-03-19 17:59:25 -0300

    feat(client): implement navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import Notifications from "./components/Notifications.vue";
+import Navbar from "@/components/Navbar.vue"
+import { useStore } from "./store";
+import { computed } from "vue";
+
+const store = useStore();
+const user = computed(() => store.state.user.user);
+
 </script>
 
 <template>
-  <notifications />
-  <RouterView />
+  <div class="background">
+    <Navbar v-if="user" />
+    <notifications />
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
+.background {
+  background-color: #292b2c;
+  color: #fff;
+  height: 100vh;
+}
 </style>
