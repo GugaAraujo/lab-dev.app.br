4b299f885d397241fd11977dc599a861d0da9bc0
Gustavo Araújo <gustavoaraujo.dev@gmail.com>
2023-03-19 12:01:43 -0300

    feature(client): implement router guards

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,23 +1,65 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import { store } from "./../store";
+import HomeView from '@/views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from "@/views/RegisterView.vue"
+import { LOGOUT } from "@/store/types-action";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
       name: 'home',
+      meta: { requireAuth: true },
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: "/register",
+      name: "register",
+      meta: { onlyGuest: true },
+      component: RegisterView,
+    },
+    {
+      path: '/login',
+      name: 'login',
+      meta: { onlyGuest: true },
+      component: LoginView
+    },
+    {
+      path: "/logout",
+      name: "logout",
+      component: {
+        beforeRouteEnter(to, from, next) {
+          store.dispatch(LOGOUT);
+          next('/login');
+        },
+      },
+    },
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  const hasToken = store.state.user?.user?.token
+  if (to.matched.some(record => record.meta.requireAuth)) {
+    if (!hasToken) {
+      next({ name: 'login' });
+    }
+    else {
+      next();
+    }
+  }
+
+  else if (to.matched.some(record => record.meta.onlyGuest)) {
+    if (hasToken) {
+      next({ name: "home" });
+    } else {
+      next();
+    }
+  }
+  else {
+    next();
+  }
 })
 
 export default router
