b280fe21b3ad02f718d7ff254456230d35075ab5
Gustavo Araújo <gustavoaraujo.dev@gmail.com>
2023-03-20 02:41:03 -0300

    fix(client): fix company card

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Pagination from "@/components/Pagination.vue"
 import http from '@/http'
 import { onMounted, ref } from 'vue'
 
@@ -31,10 +32,10 @@ const getCompanies = async (companyName = null) => {
 </script>
 
 <template>
-  <main class="container d-flex flex-column w-75 ">
+  <main class="container w-75 ">
     <div class="d-flex my-4 mx-auto">
       <input
-        class="form-control me-2"
+        class="form-control"
         v-model="filter"
         type="search"
         @input="search"
@@ -44,23 +45,27 @@ const getCompanies = async (companyName = null) => {
     </div>
 
     <div v-for="company in companies" :key="(company as any).id">
-      <div class="bg-light my-2 p-4 text-black rounded">
-        <p>
-          <span class="me-2">Empresa:</span>
-          <span>{{ (company as any)?.name }}</span>
-        </p>
-        <p>
-          <span class="me-2">Endereço:</span>
-          <span>{{ (company as any)?.address }}</span>
-        </p>
-        <p>
-          <span class="me-2">Criado em:</span>
-          <span>{{ formatDate((company as any)?.createdAt) }}</span>
-        </p>
-        <p class="me-2">Domínios:</p>
-        <ul v-for="domain in (company as any)?.domains" :key="domain">
-          <li><a :href="domain" target="_blank">{{ domain }}</a></li>
-        </ul>
+      <div class="bg-light my-2 p-4 text-black d-flex flex-collumn rounded">
+        <div class="col-6">
+          <p class="fs-5 text">
+            <span class="me-2">Empresa:</span>
+            <span>{{ (company as any)?.name }}</span>
+          </p>
+          <p>
+            <span class="me-2">Endereço:</span>
+            <span>{{ (company as any)?.address }}</span>
+          </p>
+          <p>
+            <span class="me-2">Criado em:</span>
+            <span>{{ formatDate((company as any)?.createdAt) }}</span>
+          </p>
+        </div>
+        <div class="col-6">
+          <p class="fs-5 text me-2">Domínios:</p>
+          <ul v-for="domain in (company as any)?.domains" :key="domain">
+            <li><a :href="domain" target="_blank">{{ domain }}</a></li>
+          </ul>
+        </div>
       </div>
     </div>
   </main>
