9657975d34f6866f2ba89a53c3678c9d11b0c327
Gustavo Araújo <gustavoaraujo.dev@gmail.com>
2023-03-19 23:31:25 -0300

    feat(infra): add user role information

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
+import { RoleEnum } from '@/interfaces/IUser';
 import router from '@/router'
+import { useStore } from "@/store";
+import { computed } from "vue";
+
+const store = useStore();
+const user = computed(() => store.state.user.user);
+const role = computed(() => {
+  return store.state.user.user?.role === RoleEnum.ADMIN as unknown ? 'Adminstrador' : 'Colaborador'
+});
 
 </script>
 
@@ -12,22 +21,14 @@ import router from '@/router'
       </button>
       <div class="collapse navbar-collapse" id="navbarScroll">
         <ul class="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">Link</a>
-          </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               <span class="rounded-circle bg-dark text-white p-2">LG</span>
             </a>
             <ul class="dropdown-menu  dropdown-menu-end">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li v-if="user" class="dropdown-item profile pb-2 text-truncate">
+                {{ user?.name }} - {{ role }}
+              </li>
               <li><hr class="dropdown-divider"></li>
               <li>
                 <a
@@ -50,6 +51,16 @@ import router from '@/router'
   background: linear-gradient(180deg, #e14a4f 2%, #ca4247 25%, #a8323a 100%);
   .nav-link {
     color: white;
+  }
+  .dropdown-menu {
+    width: 250px;
+    .profile {
+      cursor: default;
+      &:hover{
+        background-color: transparent;
+        color: black;
+      }
+    }
   }
 }
 </style>
--- client/src/interfaces/IUser.ts
+++ client/src/interfaces/IUser.ts
@@ -1,7 +1,13 @@
 export default interface IUser {
   id: number,
+  name: string,
   email: string,
   password: string,
   role: string,
   token: string,
 };
+
+export enum RoleEnum {
+  ADMIN = "ADMIN",
+  USER = "USER",
+}
